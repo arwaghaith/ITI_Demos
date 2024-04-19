e3fa5521139d90a5dc3add5e8d8b36bc0c2d8b60
Alaa_Ghita <alaa.ghita379@gmail.com>
2024-04-19 13:08:11 +0200

    .

--- Demo1/src/Tests/USART_Test/SCHED_TEST.c
+++ Demo1/src/Tests/USART_Test/SCHED_TEST.c
@@ -1,19 +1,21 @@
-
-#include "SYSTICK.h"
+#include "RCC.h"
+#include "Display.h"
 #include "SCHED.h"
-#include "APP_INIT/APP_Init.h"
+#include "NVIC.h"
+#include "APP_READSWITCH.h"
+#include "APP_UPDATESWITCH.h"
 
-
-int main(void)
+int main_XX(int argc, char* argv[])
 {
+    RCC_AHB1DisablePeri(PERI_AHB1_GPIOA);
+    RCC_AHB1DisablePeri(PERI_AHB1_GPIOB);
+    RCC_APB2DisablePeri(PERI_APB2_USART1);
+    RCC_APB1DisablePeri(PERI_APB1_USART2);
+    NVIC_EnableIRQ(NVIC_USART1);
 
-    
-    APP_Init();
+    Display_Init();
     SCHED_Init();
     SCHED_Start();
-    
-
-    
 
     return 0;
 }
--- Demo1/src/Tests/main.c
+++ Demo1/src/Tests/main.c
@@ -1,21 +1,18 @@
-#include "RCC.h"
-#include "Display.h"
+#include "SYSTICK.h"
 #include "SCHED.h"
-#include "NVIC.h"
-#include "APP_READSWITCH.h"
-#include "APP_UPDATESWITCH.h"
+#include "APP_INIT/APP_Init.h"
 
-int main_XX(int argc, char* argv[])
+
+int main(void)
 {
-    RCC_AHB1DisablePeri(PERI_AHB1_GPIOA);
-    RCC_AHB1DisablePeri(PERI_AHB1_GPIOB);
-    RCC_APB2DisablePeri(PERI_APB2_USART1);
-    RCC_APB1DisablePeri(PERI_APB1_USART2);
-    NVIC_EnableIRQ(NVIC_USART1);
 
-    Display_Init();
+    
+    APP_Init();
     SCHED_Init();
     SCHED_Start();
+    
+
+    
 
     return 0;
 }
